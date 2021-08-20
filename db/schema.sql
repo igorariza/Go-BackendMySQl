d4e0dbdd450fc7a8e45bd7b34772dc74a9367155
igorariza <igorariza@gmail.com>
2021-08-19 21:11:04 -0500

    feat: deploy heroku modified DB

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE `institution` (
 	`dane_id`	VARCHAR(100),	
 	`name`	VARCHAR(80),	
 	`nit_id`	VARCHAR(80),		
-PRIMARY KEY (`idInstitution`) ) ENGINE=InnoDB;
+PRIMARY KEY (`idInstitution`) ) ENGINE=InnoDB  
+DEFAULT CHARSET=utf8;
 
 CREATE TABLE `sede` ( 				
 	`idSede`	INT	AUTO_INCREMENT,
@@ -12,7 +13,8 @@ CREATE TABLE `sede` (
 	`name_sede`	VARCHAR(80),	
 PRIMARY KEY (`idSede`),
 FOREIGN KEY (`idInstitution`) 
-REFERENCES institution(`idInstitution`)) ENGINE=InnoDB;		
+REFERENCES institution(`idInstitution`)) ENGINE=InnoDB  
+DEFAULT CHARSET=utf8;			
 
 CREATE TABLE `users` ( 	
 	`idUser`	INT	AUTO_INCREMENT,
@@ -24,16 +26,17 @@ CREATE TABLE `users` (
 	`phone`	VARCHAR(20),		
 	`address`	VARCHAR(20),		
 	`photo`	VARCHAR(1000),		
-	`created_at`	VARCHAR(100),		
-	`type_id`	VARCHAR(2),		
-	`date_birth`	VARCHAR(100),		
-	`last_access`	VARCHAR(100),		
+	`created_at`	VARCHAR(50),		
+	`type_id`	INT,		
+	`date_birth`	VARCHAR(50),		
+	`last_access`	VARCHAR(50),		
 	`rh`	VARCHAR(2),		
-	`idSede`	VARCHAR(2), -- relationship Sede		
-	`is_active`	VARCHAR(2),		
+	`idSede`	INT, -- relationship Sede		
+	`is_active`	BOOLEAN,		
 PRIMARY KEY (`idUser`),
 FOREIGN KEY (`idSede`) 
-REFERENCES sede(idSede)) AUTO_INCREMENT=3;
+REFERENCES sede(idSede)) ENGINE=InnoDB AUTO_INCREMENT=3 
+DEFAULT CHARSET=utf8;
 
 -- INSERT ----
 
@@ -49,11 +52,10 @@ INSERT INTO sede
 	(`idInstitution`, `code_sede`, `name_sede`)
 VALUES
 (1,'17636400001502','MANUELA BELTRAN');
-
 -- SELECT * FROM backendSigeGo.Users;
 -- Json
 {
-    "document_id":"11223344",
+    "document_id":"112233445",
     "first_name": "Admin",
     "last_name": "SIGE",
     "email": "adminsige@gmail.com",
@@ -62,17 +64,15 @@ VALUES
     "address": "",
     "photo": "",
     "created_at": "",
-    "type_id": "",
-    "date_birth": "",
+    "type_id": "1",
+    "date_birth": "10-11-1987",
     "last_access": "",
     "rh": "",    
     "idSede": "1",
-    "is_active": ""
+    "is_active": "1"
 }	
 -- SELECT * from Users WHERE email ='adminsige@gmail.com'
-
 -- QUERY
-
 SELECT Users.idUser, Users.document_id, Users.first_name, Users.last_name, Users.email, Users.password, Users.phone, Users.address, Users.photo, Users.created_at, Users.type_id, Users.date_birth, Users.rh, Users.idSede, Users.is_active, Sede.name_sede 
 FROM Users
 INNER JOIN Sede ON Sede.idSede = Users.idSede
